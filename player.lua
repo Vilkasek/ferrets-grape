ca14d289ed7356a2010e21e57b6360611d874dfb
Jakub Brandt <tv.brandon.nr1@gmail.com>
2025-07-23 17:39:55 +0200

    Dodałem pola gracza
    W strukturze, przechowującej gracza dodałem pola, dzięki którym będzie
    gracz działał

--- player.lua
+++ player.lua
@@ -1,2 +1,22 @@
-local Player = {}
+local Player = {
+	animations = {
+		idle = {},
+		moving = {},
+		ascending = {},
+		descending = {},
+	},
+	current_animation = "idle",
+	frame = 0,
+	anim_timer = 0,
+	anim_speed = 8,
+
+	vel_x = 0,
+	vel_y = 0,
+
+	pos_x = 0,
+	pos_y = 0,
+
+	speed = 20,
+}
+
 return Player
