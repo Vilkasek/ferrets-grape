805b862fb2e3e14048e80f549568417e550073f7
Jakub Brandt <tv.brandon.nr1@gmail.com>
2025-07-23 17:12:53 +0200

    Dodałem tło
    Napisałem skrypt ładujący i wyświetlający tło

--- script.lua
+++ script.lua
@@ -1,3 +1,12 @@
+local backgrounds = {
+	"./assets/graphics/backgrounds/level1.png",
+	"./assets/graphics/backgrounds/level2.png",
+}
+
+local background = image.load(backgrounds[1])
+
 while true do
+	image.blit(background, 0, 0)
+
 	screen.flip()
 end
