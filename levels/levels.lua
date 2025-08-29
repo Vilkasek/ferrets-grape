9fe41faccc69624a227322df6debce78a7a4873f
Jakub Brandt <tv.brandon.nr1@gmail.com>
2025-08-29 18:46:12 +0200

    Zacząłem pracować nad poziomami

--- levels/levels.lua
+++ levels/levels.lua
@@ -1,20 +1,20 @@
 return {
-	[1] = {
-		map_data_path = "levels.level1",
-		deco_data_path = "levels.level1_decorations",
-		tileset_path = "./assets/graphics/tilesets/level1.png",
-		deco_tileset_path = "./assets/graphics/tilesets/grass.png",
-		background_index = 2,
-		start_pos = { x = 100, y = 100 },
-		portal_pos = { x = 32*16, y = 32*7 },
-	},
-	[2] = {
-		map_data_path = "levels.level2",
-		deco_data_path = "levels.level2_decorations",
-		tileset_path = "./assets/graphics/tilesets/level2.png",
-		deco_tileset_path = "./assets/graphics/tilesets/grass.png",
-		background_index = 3,
-		start_pos = { x = 50, y = 150 },
-		portal_pos = { x = 32*23, y = 32*5 },
-	},
+  [1] = {
+    map_data_path = "levels.level1",
+    deco_data_path = "levels.level1_decorations",
+    tileset_path = "./assets/graphics/tilesets/level1.png",
+    deco_tileset_path = "./assets/graphics/tilesets/grass.png",
+    background_index = 2,
+    start_pos = { x = 100, y = 100 },
+    portal_pos = { x = 32 * 75, y = 32 * 7 },
+  },
+  -- [2] = {
+  --   map_data_path = "levels.level2",
+  --   deco_data_path = "levels.level2_decorations",
+  --   tileset_path = "./assets/graphics/tilesets/level2.png",
+  --   deco_tileset_path = "./assets/graphics/tilesets/grass.png",
+  --   background_index = 3,
+  --   start_pos = { x = 50, y = 150 },
+  --   portal_pos = { x = 32 * 23, y = 32 * 5 },
+  -- },
 }
