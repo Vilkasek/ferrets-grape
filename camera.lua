85e0eb0e11b0e8ef96fb676c6bb330657489f131
Jakub Brandt <tv.brandon.nr1@gmail.com>
2025-09-01 18:44:40 +0200

    Dodałem nowe poziomy

--- camera.lua
+++ camera.lua
@@ -1,39 +1,39 @@
 local camera = {
-	x = 0,
-	y = 0,
-	smoothing = 0.1,
+  x = 0,
+  y = 0,
+  smoothing = 0.2,
 }
 
 local SCREEN_WIDTH = 480
 local SCREEN_HEIGHT = 272
 
 function camera.update(player, tilemap)
-	local target_x = player.pos_x + (player.width / 2) - (SCREEN_WIDTH / 2)
-	local target_y = player.pos_y + (player.height / 2) - (SCREEN_HEIGHT / 2)
+  local target_x = player.pos_x + (player.width / 2) - (SCREEN_WIDTH / 2)
+  local target_y = player.pos_y + (player.height / 2) - (SCREEN_HEIGHT / 2)
 
-	camera.x = camera.x + (target_x - camera.x) * camera.smoothing
-	camera.y = camera.y + (target_y - camera.y) * camera.smoothing
+  camera.x = camera.x + (target_x - camera.x) * camera.smoothing
+  camera.y = camera.y + (target_y - camera.y) * camera.smoothing
 
-	camera.x = math.floor(camera.x)
-	camera.y = math.floor(camera.y)
+  camera.x = math.floor(camera.x)
+  camera.y = math.floor(camera.y)
 
-	if camera.x <= 0 then
-		camera.x = 0
-	end
+  if camera.x <= 0 then
+    camera.x = 0
+  end
 
-	if camera.y <= 0 then
-		camera.y = 0
-	end
+  if camera.y <= 0 then
+    camera.y = 0
+  end
 
-	local map_width_pixels = tilemap.map_width * tilemap.tilesize
-	if camera.x + SCREEN_WIDTH >= map_width_pixels then
-		camera.x = map_width_pixels - SCREEN_WIDTH
-	end
+  local map_width_pixels = tilemap.map_width * tilemap.tilesize
+  if camera.x + SCREEN_WIDTH >= map_width_pixels then
+    camera.x = map_width_pixels - SCREEN_WIDTH
+  end
 
-	local map_height_pixels = tilemap.map_height * tilemap.tilesize
-	if camera.y + SCREEN_HEIGHT >= map_height_pixels then
-		camera.y = map_height_pixels - SCREEN_HEIGHT
-	end
+  local map_height_pixels = tilemap.map_height * tilemap.tilesize
+  if camera.y + SCREEN_HEIGHT >= map_height_pixels then
+    camera.y = map_height_pixels - SCREEN_HEIGHT
+  end
 end
 
 return camera
--- levels/levels.lua
+++ levels/levels.lua
@@ -1,11 +1,20 @@
 return {
-	[1] = {
-		map_data_path = "levels.level1",
-		deco_data_path = "levels.level1_decorations",
-		tileset_path = "./assets/graphics/tilesets/level1.png",
-		deco_tileset_path = "./assets/graphics/tilesets/grass.png",
-		background_index = 2,
-		start_pos = { x = 100, y = 100 },
-		portal_pos = { x = 32 * 59, y = 32 * 25 },
-	},
+  [1] = {
+    map_data_path = "levels.level1",
+    deco_data_path = "levels.level1_decorations",
+    tileset_path = "./assets/graphics/tilesets/level1.png",
+    deco_tileset_path = "./assets/graphics/tilesets/grass.png",
+    background_index = 2,
+    start_pos = { x = 32 * 0, y = 32 * 7 },
+    portal_pos = { x = 32 * 59, y = 32 * 25 },
+  },
+  [2] = {
+    map_data_path = "levels.level2",
+    deco_data_path = "levels.level2_decorations",
+    tileset_path = "./assets/graphics/tilesets/level2.png",
+    deco_tileset_path = "./assets/graphics/tilesets/grass.png",
+    background_index = 3,
+    start_pos = { x = 32 * 0, y = 32 * 7 },
+    portal_pos = { x = 32 * 59, y = 32 * 25 },
+  },
 }
